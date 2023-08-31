d284458dbf6ac2200e93acb1d1631c23bf56aac3
DreamingCodes <me@dreaming.codes>
2023-08-31 04:20:58 +0200

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,70 @@
+<script lang="ts" setup>
+import WelcomeItem from './WelcomeItem.vue'
+import InstagramIcon from './icons/IconInstagram.vue'
+import GoFoundMeIcon from './icons/IconGoFoundMe.vue'
+import DiscordIcon from './icons/IconDiscord.vue'
+import YoutubeIcon from './icons/IconYoutube.vue'
+import TikTokIcon from './icons/IconTikTok.vue'
+</script>
+
+<template>
+  <a href="https://www.instagram.com/rpgmaster.ai" target="_blank">
+    <WelcomeItem>
+      <template #icon>
+        <InstagramIcon/>
+      </template>
+      <template #heading>Instagram</template>
+
+      Follow us on Instagram to get the latest news and updates!
+    </WelcomeItem>
+  </a>
+
+
+  <a href="https://gofund.me/f23338aa" target="_blank">
+    <WelcomeItem>
+      <template #icon>
+        <GoFoundMeIcon/>
+      </template>
+      <template #heading>GoFoundMe</template>
+
+      Support us on GoFoundMe to make this project a reality!
+    </WelcomeItem>
+  </a>
+
+
+  <a href="https://discord.gg/DdkN5DuYQA" target="_blank">
+    <WelcomeItem>
+      <template #icon>
+        <DiscordIcon/>
+      </template>
+      <template #heading>Discord</template>
+
+      Join our Discord server to chat with the community and the team behind MasterAi!
+    </WelcomeItem>
+  </a>
+
+
+  <a href="https://www.youtube.com/@rpgMasterAi" target="_blank">
+    <WelcomeItem>
+      <template #icon>
+        <YoutubeIcon/>
+      </template>
+      <template #heading>YouTube</template>
+
+      Follow us on YouTube to get the latest news and updates!
+    </WelcomeItem>
+  </a>
+
+
+  <a href="https://www.tiktok.com/@rpgmaster.ai" target="_blank">
+    <WelcomeItem>
+      <template #icon>
+        <TikTokIcon/>
+      </template>
+      <template #heading>TikTok</template>
+
+      Follow us on TikTok to get the latest news and updates!
+    </WelcomeItem>
+  </a>
+
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <p>
+      Greetings, adventurer! I am Master AI, the groundbreaking force in role-playing games and your personal artificial intelligence Game Master.
+      Together, we journey forth to redefine the realm of RPGs.
+      With my guidance, you'll embark on limitless adventures shaped by your every decision. Not only will I craft your tale, but I'll also bring it to life with illustrations, much like a true gamebook. While others offer you multiple choices and 17,000 variations of endings, we offer the infinite.
+      You'll simply need to tell me your action, just as you would with a traditional game master in a classic tabletop role-playing game. Crafted by the fifth Edition, Defined by You.
+      Your support is the very essence that will propel this renaissance,
+      granting all players a chance to weave their own endless epics.
+      Will you stand beside us in forging the future of RPGs?"  Master.ai - the only limit?  Your imagination.
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
